5b96c9ee8b87cf7509b439e474b7da36c11daac1
Michael Archolekas <marcholekas@renoptipower.gr>
2024-01-28 12:21:22 +0200

    unique entity context

--- BlackHole.Lite/CoreSupport/EntityContextBuilder.cs
+++ BlackHole.Lite/CoreSupport/EntityContextBuilder.cs
@@ -7,27 +7,32 @@ namespace BlackHole.CoreSupport
     {
         internal void InsertEntityContext(Type EntityType)
         {
-            using (TripleStringBuilder sb = new())
-            {
-                List<string> Columns = new();
-                foreach (PropertyInfo prop in EntityType.GetProperties())
-                {
-                    if (prop.Name != "Inactive")
-                    {
-                        if (prop.Name != "Id")
-                        {
-                            sb.PNSb.Append($", {prop.Name}");
-                            sb.PPSb.Append($", @{prop.Name}");
-                            sb.UPSb.Append($",{prop.Name} = @{prop.Name}");
-                        }
-                        Columns.Add(prop.Name);
-                    }
-                }
+            BHEntityContext? existingContext = BHDataProvider.EntitiesContext.FirstOrDefault(x => x.EntityType == EntityType);
 
-                BHDataProvider.EntitiesContext.Add(new(EntityType, EntityType.CheckActivator(),
-                    EntityType.Name, Columns, $"{sb.PNSb.ToString().Remove(0, 1)} ",
-                    $"{sb.PPSb.ToString().Remove(0, 1)} ",
-                    $"{sb.UPSb.ToString().Remove(0, 1)} "));
+            if (existingContext ==  null)
+            {
+                using (TripleStringBuilder sb = new())
+                {
+                    List<string> Columns = new();
+                    foreach (PropertyInfo prop in EntityType.GetProperties())
+                    {
+                        if (prop.Name != "Inactive")
+                        {
+                            if (prop.Name != "Id")
+                            {
+                                sb.PNSb.Append($", {prop.Name}");
+                                sb.PPSb.Append($", @{prop.Name}");
+                                sb.UPSb.Append($",{prop.Name} = @{prop.Name}");
+                            }
+                            Columns.Add(prop.Name);
+                        }
+                    }
+
+                    BHDataProvider.EntitiesContext.Add(new(EntityType, EntityType.CheckActivator(),
+                        EntityType.Name, Columns, $"{sb.PNSb.ToString().Remove(0, 1)} ",
+                        $"{sb.PPSb.ToString().Remove(0, 1)} ",
+                        $"{sb.UPSb.ToString().Remove(0, 1)} "));
+                }
             }
         }
     }
